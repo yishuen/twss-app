7fc57f413865ed0d57e6b9801804dad1a19252d1
Yi Shuen Lim <limyishuen@gmail.com>
2019-10-15 14:18:43 -0400

    a;sdklfjal;ksdjf

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ import pickle
 
 import nltk
 
-nltk.data.path.append('./nltk_data/')
+# nltk.data.path.append('./nltk_data/')
 
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
